d73813f021b5c40b0ba87158dbfa8db1fb4488c1
Viennarz Curtiz <viennarzc@gmail.com>
2023-05-15 08:34:24 +0800

    Create Project - UIStateManagement

---
+++ UIStateManagement/UIStateManagement/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UIStateManagement
+//
+//  Created by Viennarz Curtiz on 5/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIStateManagement/UIStateManagement/UIStateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIStateManagementApp.swift
+//  UIStateManagement
+//
+//  Created by Viennarz Curtiz on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct UIStateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
