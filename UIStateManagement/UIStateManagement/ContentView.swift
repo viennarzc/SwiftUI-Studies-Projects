6153009aac0093c0fed52e33d475686e8e93f47d
Viennarz Curtiz <viennarzc@gmail.com>
2023-05-15 09:17:59 +0800

    Create `WithinOwnView`

--- UIStateManagement/UIStateManagement/ContentView.swift
+++ UIStateManagement/UIStateManagement/ContentView.swift
@@ -7,20 +7,62 @@
 
 import SwiftUI
 
+
+enum ViewState {
+    case loading
+    case populated
+    case empty
+    case error
+}
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            WithinOwnView()
+            .tabItem {
+                Image(systemName: "1.circle.fill")
+            }
+            
+            
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WithinOwnView: View {
+    @State private var state: ViewState = .loading
+    
+    var body: some View {
+        ZStack {
+            
+            switch state {
+            case .populated:
+                Text("Populated")
+            
+            case .empty:
+                Text("Empty")
+                
+            case .loading:
+                EmptyView()
+                
+            case .error:
+                Text("Error")
+            }
+
+            if state == .loading {
+                ProgressView()
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(Int(3.0))) {
+                state = .populated
+            }
+        }
     }
 }
